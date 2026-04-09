40b541e8053056eb6b517be67222ca84492e7b6a
celineabritha <abrithajohn@gmail.com>
2026-04-09 09:35:07 +0530

    Celine Added UC8

---
+++ UC8_FilterBogies.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
+
+public class UC8_FilterBogies {
+
+    public static void main(String[] args) {
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+
+        List<Bogie> filtered = bogies.stream()
+                .filter(b -> b.capacity > 60)
+                .collect(Collectors.toList());
+
+        for (Bogie b : filtered) {
+            System.out.println(b.name + " - " + b.capacity);
+        }
+    }
+}
